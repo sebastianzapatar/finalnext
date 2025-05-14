fbe11d63f9474c02b995a26215c90ac7fe46eec4
Sebastián Zapata <63472538+sebastianzapatar@users.noreply.github.com>
2025-05-14 14:56:42 -0500

    Se organiza para poder subir

--- app/components/chef/ChefForm.tsx
+++ app/components/chef/ChefForm.tsx
@@ -1,10 +1,5 @@
 'use client'
 
-import { Form } from "@/components/ui/form"
-import { Button } from "@/components/ui/button"
-import { ChefSchema,ChefFormValues } from "@/app/schemas/ChefFormSchema"
-import ChefFormFields from "./ChefFormValues"
-
 
 export const ChefForm = () => {
     
