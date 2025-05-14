7c2a98b65cbeaf998b2cef44f2ec68fc7ae4399c
Sebastián Zapata <63472538+sebastianzapatar@users.noreply.github.com>
2025-05-14 15:14:23 -0500

    Se organiza again

--- app/(general)/chef/[id]/page.tsx
+++ app/(general)/chef/[id]/page.tsx
@@ -1,10 +1,12 @@
 import { ChefDetail } from "@/app/components/chef/ChefDetail";
-interface Props{
-    params:{id:string}
-}
-export default async function ChefDetailPage({params}:Props) {
-    return(
-    <ChefDetail id={params.id}/>
+import { Metadata } from "next";
 
-    )
+type Props = {
+  params: {
+    id: string;
+  };
+};
+
+export default function ChefDetailPage({ params }: Props) {
+  return <ChefDetail id={params.id} />;
 }
